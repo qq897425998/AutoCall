7844055c7642dace8039c8f69c4a7b2c085ab382
唐鑫 <897425998@qq.com>
2017-01-08 23:51:47 +0800

    create project

---
+++ app/src/main/java/com/github/qq897425998/autocall/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.qq897425998.autocall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
